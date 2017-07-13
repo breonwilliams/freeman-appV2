782f4568add3a2d55e529ad2030d11049ee9a751
breonwilliams <bjwilli83@gmail.com>
2017-07-13 16:27:44 -0400

    modify text

--- plugins/bower_components/site-loader/js/site-loader-init.js
+++ plugins/bower_components/site-loader/js/site-loader-init.js
@@ -2,7 +2,7 @@
  * Created by FMS on 6/30/17.
  */
 
-$("#siteloader").html('<object data="https://www.forex.com/en-us/open-an-account" />');
+$("#siteloader").html('<object data="https://apply.forex.com/NewOLS_FRXC_EN/applyforex.aspx?start=n26457&bc=mqus&ibc=frxc&sibc=mqus" />');
 
 $("#siteloaderDeposit").html('<object data="https://myaccount.forex.com/Home.aspx?docId=1,0" />');
 
